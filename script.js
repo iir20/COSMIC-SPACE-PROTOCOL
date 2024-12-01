633e1af52bcc9227daeb126934070ed9dd50193a
iir20 <ismailratul340@gmail.com>
2024-12-01 22:11:48 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.querySelector('.wallet-connect').addEventListener('click', () => {
+    alert('Wallet connection feature coming soon!');
+});
+
+document.querySelector('.access-btn').addEventListener('click', () => {
+    alert('Accessing the Cosmic World...');
+});
